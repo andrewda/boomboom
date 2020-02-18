0954a5b9edb908215f36acaae2646963e30e0069
Andrew Dassonville <dassonville.andrew@gmail.com>
2020-02-17 23:34:51 -0800

    Add additional logging

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import json
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-config = json.load(open('config.json'))
+config = json.load(open('/home/andrewda/boomboom/config.json'))
 
 api = twitter.Api(consumer_key=config['consumer_key'],
                   consumer_secret=config['consumer_secret'],
                   access_token_key=config['access_token_key'],
                   access_token_secret=config['access_token_secret'])
 
-tweet = """
+update_tweet = """
 Update @ {time}
 
 Chicken Bites:
@@ -40,7 +40,7 @@ negative_icon = '❌ '
 def get_icon(value):
     return positive_icon if value else negative_icon
 
-status_file = open('status.json', 'r+')
+status_file = open('/home/andrewda/boomboom/status.json', 'r+')
 prev_status = {}
 
 try:
@@ -56,21 +56,26 @@ new_status = {
 }
 
 if res.status_code == 200 and (not 'open' in prev_status or not prev_status['open']):
-    api.PostUpdate(open_tweet.format(time=datetime.now().strftime('%Y-%m-%d %H:%M')))
+    tweet = open_tweet.format(time=datetime.now().strftime('%Y-%m-%d %H:%M'))
+    print(tweet)
+    api.PostUpdate(tweet)
 elif res.status_code == 302 and (not 'open' in prev_status or prev_status['open']):
-    api.PostUpdate(closed_tweet.format(time=datetime.now().strftime('%Y-%m-%d %H:%M')))
+    tweet = closed_tweet.format(time=datetime.now().strftime('%Y-%m-%d %H:%M'))
+    print(tweet)
+    api.PostUpdate(tweet)
+
+status_file.seek(0)
+json.dump(new_status, status_file)
+status_file.truncate()
 
 if res.status_code == 302:
-    status_file.seek(0)
-    json.dump(new_status, status_file)
-    status_file.truncate()
-    print('Food2You is closed')
+    print('[DEBUG {}] Food2You is closed'.format(datetime.now().strftime('%Y-%m-%d %H:%M')))
     exit(1)
 
 soup = BeautifulSoup(res.content, 'html.parser')
 
 items = [soup.find(class_=name) for name in ['ChickenBitesw/BoomBoomsauce', 'ChickenBitesw/BBQsauce', 'ChickenBitesw/BoomBoomandBBQsauce', '4Breadsticks', '3FreshBakedCookies']]
-busy_items = list(map(lambda element: (element.find(class_='busy') or element.find(class_='out'), items) is None))
+busy_items = list(map(lambda element: (element.find(class_='busy') or element.find(class_='out')) is None, items))
 
 new_status['items'] = busy_items
 
@@ -78,12 +83,16 @@ status_file.seek(0)
 json.dump(new_status, status_file)
 status_file.truncate()
 
-if status['items'] != busy_items:
-    api.PostUpdate(tweet.format(
+if prev_status['items'] != busy_items:
+    tweet = update_tweet.format(
         time=datetime.now().strftime('%Y-%m-%d %H:%M'),
         boom=get_icon(busy_items[0]),
         bbq=get_icon(busy_items[1]),
         boom_bbq=get_icon(busy_items[2]),
         breadsticks=get_icon(busy_items[3]),
         cookies=get_icon(busy_items[4]),
-    ))
+    )
+    print(tweet)
+    api.PostUpdate(tweet)
+else:
+    print('[DEBUG {}] No updates'.format(datetime.now().strftime('%Y-%m-%d %H:%M')))
